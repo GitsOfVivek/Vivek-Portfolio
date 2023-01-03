8015d946cc5e4a4a21cbf7c01cd078eecba3be04
GitsOfVivek <singhvivek1999@yahoo.com>
2023-01-03 19:53:04 +0530

    nav update

--- js/app.js
+++ js/app.js
@@ -1,21 +1,28 @@
-// const span = document.getElementById('theme');
-// const icon = document.getElementById('theme-icon');
-// const theme = document.querySelector('body');
-// span.addEventListener('click', function () {
-// 	icon.classList.toggle('fa-moon');
-// 	if (theme.classList.contains('dark-mode')) {
-// 		theme.classList.add('light-mode');
-// 		theme.classList.remove('dark-mode');
-// 	} else {
-// 		theme.classList.remove('light-mode');
-// 		theme.classList.add('dark-mode');
-// 	}
-// });
-
 const toggle = document.getElementById('toggle');
 const theme = document.querySelector('body');
 const icon = document.getElementById('icon');
 const logoImg = document.getElementById('logo-img');
+let isActive = false;
+const hamburger = document.querySelector('.nav__hamburger');
+
+const bar = document.querySelector('.checkbtn i');
+bar.addEventListener('click', function () {
+	if (bar.classList.contains('fas', 'fa-bars')) {
+		bar.classList.add('fa-solid', 'fa-xmark');
+		bar.classList.remove('fas', 'fa-bars');
+		isActive = true;
+	} else {
+		bar.classList.remove('fa-solid', 'fa-xmark');
+		bar.classList.add('fas', 'fa-bars');
+		isActive = false;
+	}
+	if (!isActive) {
+		hamburger.style.opacity = '0';
+	} else {
+		hamburger.style.opacity = '0.95';
+	}
+});
+
 toggle.onclick = function () {
 	toggle.classList.toggle('active');
 	icon.classList.toggle('fa-moon');
